8371107d50a37df7fd33c9f781b889276766459b
aajuy <obj.property@gmail.com>
2023-10-22 16:06:28 +0900

    feat: NetworkLibrary

---
+++ NetworkLibrary/Listener.cs
@@ -0,0 +1,45 @@
+﻿using System.Diagnostics;
+using System.Net;
+using System.Net.Sockets;
+
+namespace NetworkLibrary
+{
+    public class Listener
+    {
+        Socket listenSocket;
+        Func<Session> sessionFactory;
+
+        public void Start(IPEndPoint endPoint, int backlog, Func<Session> sessionFactory)
+        {
+            this.listenSocket = new Socket(endPoint.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+            this.sessionFactory += sessionFactory;
+
+            listenSocket.Bind(endPoint);
+            listenSocket.Listen(backlog);
+
+            AcceptLoop();
+        }
+
+        async Task AcceptLoop()
+        {
+            while (true)
+            {
+                try
+                {
+                    Socket clientSocket = await listenSocket.AcceptAsync();
+                    OnAccepted(clientSocket);
+                }
+                catch (Exception ex)
+                {
+                    Debug.WriteLine(ex.ToString());
+                }
+            }
+        }
+
+        void OnAccepted(Socket clientSocket)
+        {
+            Session session = sessionFactory.Invoke();
+            session.Start(clientSocket);
+        }
+    }
+}
